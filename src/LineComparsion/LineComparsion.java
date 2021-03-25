e773348a7937f1d6316fc234432001b2dd0089f6
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-25 17:48:58 +0530

    ADDED: Added Comapre To Method is used

--- src/LineComparsion/LineComparsion.java
+++ src/LineComparsion/LineComparsion.java
@@ -2,13 +2,17 @@ package LineComparsion;
 
 import java.util.Scanner;
 
+//Method is creating 
 public class LineComparsion {
-	public static void main(String[] args) {
+	public  void  LineComparsion(){
 		System.out.println("Welcome to Line Comparsion Problem");
-
+		
+		//variables
 		int x1, x2, y1, y2,p1,p2,q1,q2;
 		double Length,Length1;
 		Scanner sc = new Scanner(System.in);
+		
+		//gives 1st lines end pionts from user
 		System.out.println("Enter x1 point");
 		x1 = sc.nextInt();
 		System.out.println("Enter y1 point");
@@ -21,7 +25,7 @@ public class LineComparsion {
 		System.out.println("Length of Line is: " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")" + Length);
 
 		
-		
+		//gives 2nd lines end pionts from user
 		System.out.println("Enter p1 point");
 		p1 = sc.nextInt();
 		System.out.println("Enter q1 point");
@@ -38,13 +42,23 @@ public class LineComparsion {
 			System.out.println("Line is not equal to Line1");
 		}
 
-		if (Length == Length1) {
-			System.out.println("Line is equal to Line1");
-		} else if (Length > Length1) {
-			System.out.println("Line is greater than Line1");
+		
+		Double line1 = Double.valueOf(Length);
+		Double line2 = Double.valueOf(Length1);
+		int val = line1.compareTo(line2);
+		if (val == 0) {
+			System.out.println("Line1 is equal to Line2");
+		} else if (val > 0) {
+			System.out.println("Line1 is greater than Line2");
 		} else {
-			System.out.println("Line is less than Line1");
+			System.out.println("Line1 is less than Line2");
 
 		}
+	
+			
+	}
+	public static void main(String[] args) {
+		LineComparsion lc = new LineComparsion();
+		lc.LineComparsion();
 	}
 }
