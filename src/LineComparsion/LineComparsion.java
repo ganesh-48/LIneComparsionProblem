73c580d2e96745a6ddd339b8d44b8fb6ebc57fba
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-25 17:16:52 +0530

    ADDED:Added Compare Two Lines based on end points and check it's equal, less tan or not equal

--- src/LineComparsion/LineComparsion.java
+++ src/LineComparsion/LineComparsion.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class LineComparsion {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparsion Problem");
-	
-		int x1, x2, y1, y2;
-		double Length;
+
+		int x1, x2, y1, y2,p1,p2,q1,q2;
+		double Length,Length1;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter x1 point");
 		x1 = sc.nextInt();
@@ -17,20 +17,11 @@ public class LineComparsion {
 		x2 = sc.nextInt();
 		System.out.println("Enter y2 point");
 		y2 = sc.nextInt();
-		Length = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-		System.out.println("Length of Line is: " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")" 	+ Length);
-	
+		Length= Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		System.out.println("Length of Line is: " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")" + Length);
+
 		
-		int p1,p2,q2,q1;
-		double Length1;
-		System.out.println("Enter x1 point");
-		x1 = sc.nextInt();
-		System.out.println("Enter y1 point");
-		y1 = sc.nextInt();
-		System.out.println("Enter x2 point");
-		x2 = sc.nextInt();
-		System.out.println("Enter y2 point");
-		y2 = sc.nextInt();
+		
 		System.out.println("Enter p1 point");
 		p1 = sc.nextInt();
 		System.out.println("Enter q1 point");
@@ -41,25 +32,19 @@ public class LineComparsion {
 		q2 = sc.nextInt();
 		Length = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
 		Length1 = Math.sqrt((p2 - p1) ^ 2 + (q2 - q1) ^ 2);
-		//System.out.println("Length of Line is: " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")" 	+ Length);
-		if(Length == Length1) {
-			System.out.println("Length is equal to Length1");
-		}
-		else {
-			System.out.println("Length is not equal to Length1");
-		}
-	
-
-		if(Length == Length1) {
-			System.out.println("Length is equal to Length1");
-		}
-		else if(Length > Length1) {
-			System.out.println("Length is greater than Length1");
-		}
-		else {
-			System.out.println("Length is less than Length1");
-
+		if (Length == Length1) {
+			System.out.println("Line is equal to Line1");
+		} else {
+			System.out.println("Line is not equal to Line1");
 		}
 
+		if (Length == Length1) {
+			System.out.println("Line is equal to Line1");
+		} else if (Length > Length1) {
+			System.out.println("Line is greater than Line1");
+		} else {
+			System.out.println("Line is less than Line1");
+
+		}
 	}
 }
